65c928f20cef4ab869a421acc260dfd50b902b4b
pati.pik <pati.pik@73fb50ce-e502-64b5-abbe-fc10fc176274>
2010-11-25 16:47:02 +0000

    Add more db.

--- trunk/mysite/weight/clamplift_views.py
+++ trunk/mysite/weight/clamplift_views.py
@@ -20,142 +20,161 @@ def clamplift(request):
 	try:
 #		HOST = '192.41.170.55' # CSIM network
 #		HOST = '192.168.101.55' # Likitomi network
-		HOST = '192.168.1.55' # My own local network: Linksys
-		PORT = 50007
-		soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		soc.settimeout(2)
-		soc.connect((HOST, PORT))
-		## soc.send('setup.operating_mode = standby\r\n')
-		soc.send('tag.db.scan_tags(1000)\r\n')
-		datum = soc.recv(128)
+#		HOST = '192.168.1.55' # My own local network: Linksys
+#		PORT = 50007
+#		soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#		soc.settimeout(2)
+#		soc.connect((HOST, PORT))
+#		## soc.send('setup.operating_mode = standby\r\n')
+#		soc.send('tag.db.scan_tags(1000)\r\n')
+#		datum = soc.recv(128)
 
-		if datum.find("ok") > -1:
-			soc.send('tag.read_id()\r\n')
-			data = soc.recv(8192)
-			tagdata = data.split("\r\n")
+#		if datum.find("ok") > -1:
+#			soc.send('tag.read_id()\r\n')
+#			data = soc.recv(8192)
+#			tagdata = data.split("\r\n")
 
-		idlist = list()
-		loclist = list()
+#		idlist = list()
+#		loclist = list()
 
-		for tag in tagdata:
-			if "AAAA" in tag:
-				idlist.append(tag)
-			if "BBBB" in tag:
-				loclist.append(tag)
+#		for tag in tagdata:
+#			if "AAAA" in tag:
+#				idlist.append(tag)
+#			if "BBBB" in tag:
+#				loclist.append(tag)
 
-		cnt = 0
-		## error = cStringIO.StringIO()
+#		cnt = 0
+#		## error = cStringIO.StringIO()
 
-		tagid_A = list()
-		type_A = list()
-		antenna_A = list()
-		repeat_A = list()
+#		tagid_A = list()
+#		type_A = list()
+#		antenna_A = list()
+#		repeat_A = list()
 
-		for id1 in idlist:
-			id2 = id1.replace("(","")
-			id2 = id2.replace(")","")
-			id3 = id2.split(", ")
-			for id4 in id3:
-				id5 = id4.split("=")
-				if id5[0]=="tag_id":tagid_A.append(id5[1])
-				elif id5[0]=="type":type_A.append(id5[1])
-				elif id5[0]=="antenna": antenna_A.append(id5[1])
-				elif id5[0]=="repeat": repeat_A.append(id5[1])
-				cnt= cnt+1
+#		for id1 in idlist:
+#			id2 = id1.replace("(","")
+#			id2 = id2.replace(")","")
+#			id3 = id2.split(", ")
+#			for id4 in id3:
+#				id5 = id4.split("=")
+#				if id5[0]=="tag_id":tagid_A.append(id5[1])
+#				elif id5[0]=="type":type_A.append(id5[1])
+#				elif id5[0]=="antenna": antenna_A.append(id5[1])
+#				elif id5[0]=="repeat": repeat_A.append(id5[1])
+#				cnt= cnt+1
 
-		tagid_B = list()
-		type_B = list()
-		antenna_B = list()
-		repeat_B = list()
+#		tagid_B = list()
+#		type_B = list()
+#		antenna_B = list()
+#		repeat_B = list()
 
-		for loc1 in loclist:
-			loc2 = loc1.replace("(","")
-			loc2 = loc2.replace(")","")
-			loc3 = loc2.split(", ")
-			for loc4 in loc3 :
-				loc5 = loc4.split("=")
-				if loc5[0]=="tag_id": tagid_B.append(loc5[1])
-				elif loc5[0]=="type": type_B.append(loc5[1])
-				elif loc5[0]=="antenna": antenna_B.append(loc5[1])
-				elif loc5[0]=="repeat": repeat_B.append(loc5[1])
-				cnt= cnt+1
+#		for loc1 in loclist:
+#			loc2 = loc1.replace("(","")
+#			loc2 = loc2.replace(")","")
+#			loc3 = loc2.split(", ")
+#			for loc4 in loc3 :
+#				loc5 = loc4.split("=")
+#				if loc5[0]=="tag_id": tagid_B.append(loc5[1])
+#				elif loc5[0]=="type": type_B.append(loc5[1])
+#				elif loc5[0]=="antenna": antenna_B.append(loc5[1])
+#				elif loc5[0]=="repeat": repeat_B.append(loc5[1])
+#				cnt= cnt+1
 
-		lan = 0
-		pos = 0
-		totalCount = 0
+#		lan = 0
+#		pos = 0
+#		totalCount = 0
 
-		if len(repeat_B) > 0 :
-			cnt = 0
-			for rep in repeat_B:
-				if type_B[cnt] == "ISOC":
-					lindex = int(tagid_B[cnt][26:28])
-					pindex = int(tagid_B[cnt][28:30])
-					lan += float(lindex)*float(repeat_B[cnt])
-					pos += float(pindex)*float(repeat_B[cnt])
-					totalCount += float(repeat_B[cnt])
+#		if len(repeat_B) > 0 :
+#			cnt = 0
+#			for rep in repeat_B:
+#				if type_B[cnt] == "ISOC":
+#					lindex = int(tagid_B[cnt][26:28])
+#					pindex = int(tagid_B[cnt][28:30])
+#					lan += float(lindex)*float(repeat_B[cnt])
+#					pos += float(pindex)*float(repeat_B[cnt])
+#					totalCount += float(repeat_B[cnt])
 
-				cnt = cnt+1
+#				cnt = cnt+1
 
-		if totalCount > 0:
-			L = int(round(lan/totalCount,0))
-			P = int(round(pos/totalCount,0))
-		else:
-			L = 0
-			P = 0
+#		if totalCount > 0:
+#			L = int(round(lan/totalCount,0))
+#			P = int(round(pos/totalCount,0))
+#		else:
+#			L = 0
+#			P = 0
 
-		atlane = str(L)
-		atposition = str(P)
-		atlocation = ''
+#		atlane = str(L)
+#		atposition = str(P)
+#		atlocation = ''
 
-		if L == 0:
-			atlocation = 'CR'
-		if L == 5 and P == 5:
-			atlocation = 'Scale'
-		if L in range(1, 5):
-			atlocation = 'Stock'
+#		if L == 0:
+#			atlocation = 'CR'
+#		if L == 5 and P == 5:
+#			atlocation = 'Scale'
+#		if L in range(1, 5):
+#			atlocation = 'Stock'
 
-		if L == 0 and P == 0:
-			atlane = ""
-			atposition = ""
-			atlocation = ""
-			toperror = "[No location tag in field.]"
+#		if L == 0 and P == 0:
+#			atlane = ""
+#			atposition = ""
+#			atlocation = ""
+#			toperror = "[No location tag in field.]"
 
-		repeat_AA = list()
+#		repeat_AA = list()
 
-		for rep_A in repeat_A:
-			repeat_AA.append(int(rep_A))
+#		for rep_A in repeat_A:
+#			repeat_AA.append(int(rep_A))
 
-		if max(repeat_AA) in repeat_AA:
-			n = repeat_AA.index(max(repeat_AA))
+#		if max(repeat_AA) in repeat_AA:
+#			n = repeat_AA.index(max(repeat_AA))
 
-		tagsplt = tagid_A[n].split("AAAA")
-		realtag = int(tagsplt[1][0:4])
+#		tagsplt = tagid_A[n].split("AAAA")
+#		realtag = int(tagsplt[1][0:4])
 
-		soc.close()
+#		soc.close()
 
-#		atlane = 5
-#		atposition = 5
-#		atlocation = 'Scale'
+		atlane = '5'
+		atposition = '5'
+		atlocation = 'Scale'
 
-#		realtag = 68
+#		atlane = '1'
+#		atposition = '13'
+#		atlocation = 'Stock'
+
+		realtag = 64
 
 # Query database #
-		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-		cur = conn.cursor()
+#		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
+#		cur = conn.cursor()
 
-		cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", realtag)
-		query1 = cur.fetchone()
+#		cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", realtag)
+#		query1 = cur.fetchone()
+#		paper_roll_id = query1[0]
+#		paper_code = query1[1]
+#		initial_weight = query1[4]
+#		size = query1[7]
+#		uom = query1[8]
+#		temp_weight = query1[17]
+#		lane = query1[18]
+#		position = query1[19]
+#		uppos = int(position)+1
+#		downpos = int(position)-1
+#		digital = str(temp_weight)
+	
+		query = PaperRoll.objects.filter(id=realtag).values_list('id', 'paper_code', 'width', 'initial_weight','temp_weight', 'lane', 'position')[0]
+		query1 = list(query)
+
 		paper_roll_id = query1[0]
 		paper_code = query1[1]
-		initial_weight = query1[4]
-		size = query1[7]
-		uom = query1[8]
-		temp_weight = query1[17]
-		lane = query1[18]
-		position = query1[19]
+		size = query1[2]
+		initial_weight = query1[3]
+		temp_weight = query1[4]
+		lane = query1[5]
+		position = query1[6]
 		uppos = int(position)+1
 		downpos = int(position)-1
-		digital = str(temp_weight)
+		digital = str(temp_weight)+".0"
+		lend = len(digital)
 
 		if lane == 'A':
 			opplane = 'B'
@@ -225,17 +244,27 @@ def clamplift(request):
 			digit6 = digital[1:2]
 			digit7 = digital[2:3]
 
-		cur.execute("SELECT * FROM `paper_movement` WHERE `paper_movement`.`roll_id` = %s ORDER BY `paper_movement`.`created_on` DESC", realtag)
-		query2 = cur.fetchall()
+#		cur.execute("SELECT * FROM `paper_movement` WHERE `paper_movement`.`roll_id` = %s ORDER BY `paper_movement`.`created_on` DESC", realtag)
+#		query2 = cur.fetchall()
 
-		if len(query2)>0:
-			actual_wt = query2[0][3]
+#		if len(query2)>0:
+#			actual_wt = query2[0][3]
+#		else:
+#			actual_wt = initial_weight
+#			undo_btn = ""
+
+		query222 = PaperHistory.objects.filter(roll_id=realtag).exists()
+
+		if query222 == True:
+			query22 = PaperHistory.objects.filter(roll_id=realtag).order_by('-timestamp').values_list('last_wt')[0]
+			query2 = list(query22)
+			actual_wt = query2[0]
 		else:
 			actual_wt = initial_weight
 			undo_btn = ""
 
-		cur.close()
-		conn.close()
+#		cur.close()
+#		conn.close()
 
 # Exceptions #
 	except UnboundLocalError:
@@ -324,24 +353,36 @@ def update(request):
 	else:
 		return HttpResponseRedirect('/clamplift/')
 
-	conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-	cur = conn.cursor()
+#	conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
+#	cur = conn.cursor()
 
-	cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s LIMIT 1", realtag)
-	query1 = cur.fetchone()
+#	cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s LIMIT 1", realtag)
+#	query1 = cur.fetchone()
+
+	query11 = PaperRoll.objects.filter(id=realtag).values_list('id', 'paper_code', 'initial_weight', 'width', 'wunit', 'temp_weight')[0]
+	query1 = list(query11)
+
 	paper_roll_id = query1[0]
 	paper_code = query1[1]
-	initial_weight = query1[4]
-	size = query1[7]
-	uom = query1[8]
-	temp_weight = query1[17]
+	initial_weight = query1[2]
+	size = query1[3]
+	uom = query1[4]
+	temp_weight = query1[5]
 
-	cur.execute("SELECT * FROM `paper_movement` WHERE `paper_movement`.`roll_id` = %s ORDER BY `paper_movement`.`created_on` DESC", realtag)
-	query2 = cur.fetchall()
-	if len(query2)>0:
-		actual_wt = query2[0][3]
+#	cur.execute("SELECT * FROM `paper_movement` WHERE `paper_movement`.`roll_id` = %s ORDER BY `paper_movement`.`created_on` DESC", realtag)
+#	query2 = cur.fetchall()
+
+	query222 = PaperHistory.objects.filter(roll_id=realtag).exists()
+
+	if query222 == True:
+		query22 = PaperHistory.objects.filter(roll_id=realtag).order_by('-timestamp').values_list('last_wt')[0]
+		query2 = list(query22)
+		actual_wt = query2[0]
 	else:
 		actual_wt = initial_weight
+		undo_btn = ""
+
+	int_weight = int(weight)
 
 	try:
 		f_weight = float(weight)
@@ -352,16 +393,19 @@ def update(request):
 	if actual_wt > f_weight:
 		rightnow = datetime.datetime.now()
 		dt = rightnow.strftime("%Y-%m-%d %H:%M:%S")
-		cur.execute("INSERT INTO `likitomi_v8`.`paper_movement` (roll_id, before_wt, actual_wt, created_on) VALUES (%s, %s, %s, %s)", (realtag, actual_wt, f_weight, dt))
-		conn.commit()
+#		cur.execute("INSERT INTO `likitomi_v8`.`paper_movement` (roll_id, before_wt, actual_wt, created_on) VALUES (%s, %s, %s, %s)", (realtag, actual_wt, f_weight, dt))
+#		conn.commit()
+		p = PaperHistory(roll_id=realtag, before_wt=actual_wt, last_wt=int_weight)
+		p.save()
+		transaction.commit()
 
 	else:
 		err = "w"
 		error = "Your submitted weight is not less than previous weight."
 		return render_to_response('submit_error.html', locals())
 
-	cur.close()
-	conn.close()
+#	cur.close()
+#	conn.close()
 
 	return HttpResponseRedirect('/clamplift/')
 
@@ -373,24 +417,40 @@ def undo(request):
 	else:
 		return HttpResponseRedirect('/clamplift/')
 
-	conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-	cur = conn.cursor()
+#	conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
+#	cur = conn.cursor()
 
-	cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s LIMIT 1", realtag)
-	query1 = cur.fetchone()
+#	cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s LIMIT 1", realtag)
+#	query1 = cur.fetchone()
+#	paper_roll_id = query1[0]
+#	paper_code = query1[1]
+#	initial_weight = query1[4]
+#	size = query1[7]
+#	uom = query1[8]
+#	temp_weight = query1[17]
+
+#	cur.execute("SELECT MAX(created_on) FROM `likitomi_v8`.`paper_movement` WHERE `paper_movement`.`roll_id` = %s", realtag)
+#	query2 = cur.fetchone()
+#	max_datetime = query2[0]
+
+#	cur.execute("DELETE FROM `likitomi_v8`.`paper_movement` WHERE `paper_movement`.`roll_id` = %s AND `paper_movement`.`created_on` = %s", (realtag, max_datetime))
+#	conn.commit()
+
+	query11 = PaperRoll.objects.filter(id=realtag).values_list('id', 'paper_code', 'initial_weight', 'width', 'wunit', 'temp_weight')[0]
+	query1 = list(query11)
+
 	paper_roll_id = query1[0]
 	paper_code = query1[1]
-	initial_weight = query1[4]
-	size = query1[7]
-	uom = query1[8]
-	temp_weight = query1[17]
+	initial_weight = query1[2]
+	size = query1[3]
+	uom = query1[4]
+	temp_weight = query1[5]
 
-	cur.execute("SELECT MAX(created_on) FROM `likitomi_v8`.`paper_movement` WHERE `paper_movement`.`roll_id` = %s", realtag)
-	query2 = cur.fetchone()
-	max_datetime = query2[0]
+	p = PaperHistory.objects.filter(roll_id=realtag).order_by('-timestamp')[0]
+	p.delete()
+	transaction.commit()
 
-	cur.execute("DELETE FROM `likitomi_v8`.`paper_movement` WHERE `paper_movement`.`roll_id` = %s AND `paper_movement`.`created_on` = %s", (realtag, max_datetime))
-	conn.commit()
+#	transaction.rollback()
 
 	return HttpResponseRedirect('/clamplift/')
 
@@ -413,16 +473,32 @@ def changeloc(request):
 		return HttpResponseRedirect('/clamplift/')
 
 	if int(ipos) <= 43:
-		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-		cur = conn.cursor()
+#		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
+#		cur = conn.cursor()
 
-		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `lane` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ilane, realtag))
-		conn.commit()
-		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `position` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ipos, realtag))
-		conn.commit()
+#		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `lane` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ilane, realtag))
+#		conn.commit()
+#		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `position` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ipos, realtag))
+#		conn.commit()
+		query = PaperRoll.objects.filter(id=realtag).values_list('paper_code', 'width', 'wunit', 'initial_weight', 'temp_weight')[0]
+		qlist = list(query)
+		paper_code = qlist[0]
+		width = qlist[1]
+		wunit = qlist[2]
+		initial_weight = qlist[3]
+		temp_weight = qlist[4]
+		p = PaperRoll(id=realtag, width=width, wunit=wunit, initial_weight=initial_weight, temp_weight=temp_weight)
+		p.paper_code = paper_code
+		p.width = width
+		p.wunit = wunit
+		p.initial_weight = initial_weight
+		p.temp_weight = temp_weight
+		p.lane = ilane
+		p.position = ipos
+		p.save()
 
-		cur.close()
-		conn.close()
+#		cur.close()
+#		conn.close()
 	else:
 		err = ""
 		error = "Your submitted position is not between 1 and 43."
@@ -885,16 +961,32 @@ def minchangeloc(request):
 		return HttpResponseRedirect('/minclamp/')
 
 	if int(ipos) <= 43:
-		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-		cur = conn.cursor()
+#		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
+#		cur = conn.cursor()
 
-		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `lane` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ilane, realtag))
-		conn.commit()
-		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `position` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ipos, realtag))
-		conn.commit()
+#		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `lane` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ilane, realtag))
+#		conn.commit()
+#		cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `position` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (ipos, realtag))
+#		conn.commit()
+		query = PaperRoll.objects.filter(id=realtag).values_list('paper_code', 'width', 'wunit', 'initial_weight', 'temp_weight')[0]
+		qlist = list(query)
+		paper_code = qlist[0]
+		width = qlist[1]
+		wunit = qlist[2]
+		initial_weight = qlist[3]
+		temp_weight = qlist[4]
+		p = PaperRoll(id=realtag, width=width, wunit=wunit, initial_weight=initial_weight, temp_weight=temp_weight)
+		p.paper_code = paper_code
+		p.width = width
+		p.wunit = wunit
+		p.initial_weight = initial_weight
+		p.temp_weight = temp_weight
+		p.lane = ilane
+		p.position = ipos
+		p.save()
 
-		cur.close()
-		conn.close()
+#		cur.close()
+#		conn.close()
 	else:
 		err = ""
 		error = "Your submitted position is not between 1 and 43."
