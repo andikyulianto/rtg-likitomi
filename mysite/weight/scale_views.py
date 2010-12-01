af208fd29ad216f29079c86a40dcb9e0994fb156
pati.pik <pati.pik@73fb50ce-e502-64b5-abbe-fc10fc176274>
2010-12-01 13:58:25 +0000

    Almost cleaned.

--- mysite/weight/scale_views.py
+++ mysite/weight/scale_views.py
@@ -1,17 +1,9 @@
 # Create your views here.
-from django.template.loader import get_template
 from django.shortcuts import render_to_response
-from django.template import Template, Context
-from django.http import Http404, HttpResponse, HttpResponseRedirect
-from django.db import connection, transaction
+from weight.models import PaperRoll, PaperHistory
 
-import datetime
 import serial
-import MySQLdb
-import Image
 import socket
-import StringIO
-import cStringIO
 import random
 
 def scale(request):
@@ -79,102 +71,102 @@ def scale(request):
 			serror = "[Data sent is not complete.]"
 
 # Connect RFID reader #
-		realtag = 'None'
+#		realtag = 'None'
 
-#		HOST = '192.41.170.55' # CSIM network
-#		HOST = '192.168.101.55' # Likitomi network
-		HOST = '192.168.1.55' # My own local network: Linksys
-		PORT = 50007
-		soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		soc.settimeout(2)
-		soc.connect((HOST, PORT))
-		## soc.send('setup.operating_mode = standby\r\n')
-		soc.send('tag.db.scan_tags(1000)\r\n')
-		datum = soc.recv(128)
+##		HOST = '192.41.170.55' # CSIM network
+##		HOST = '192.168.101.55' # Likitomi network
+#		HOST = '192.168.1.55' # My own local network: Linksys
+#		PORT = 50007
+#		soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#		soc.settimeout(2)
+#		soc.connect((HOST, PORT))
+#		## soc.send('setup.operating_mode = standby\r\n')
+#		soc.send('tag.db.scan_tags(1000)\r\n')
+#		datum = soc.recv(128)
 
-		if datum.find("ok") > -1:
-			soc.send('tag.read_id()\r\n')
-			data = soc.recv(8192)
-			tagdata = data.split("\r\n")
+#		if datum.find("ok") > -1:
+#			soc.send('tag.read_id()\r\n')
+#			data = soc.recv(8192)
+#			tagdata = data.split("\r\n")
 
-		idlist = list()
-		loclist = list()
+#		idlist = list()
+#		loclist = list()
 
-		for tag in tagdata:
-			if "AAAA" in tag:
-				idlist.append(tag)
-			if "BBBB" in tag:
-				loclist.append(tag)
+#		for tag in tagdata:
+#			if "AAAA" in tag:
+#				idlist.append(tag)
+#			if "BBBB" in tag:
+#				loclist.append(tag)
 
-		cnt = 0
-		## error = cStringIO.StringIO()
+#		cnt = 0
 
-		tagid_A = list()
-		type_A = list()
-		antenna_A = list()
-		repeat_A = list()
+#		tagid_A = list()
+#		type_A = list()
+#		antenna_A = list()
+#		repeat_A = list()
 
-		for id1 in idlist:
-			id2 = id1.replace("(","")
-			id2 = id2.replace(")","")
-			id3 = id2.split(", ")
-			for id4 in id3:
-				id5 = id4.split("=")
-				if id5[0]=="tag_id":tagid_A.append(id5[1])
-				elif id5[0]=="type":type_A.append(id5[1])
-				elif id5[0]=="antenna": antenna_A.append(id5[1])
-				elif id5[0]=="repeat": repeat_A.append(id5[1])
-				cnt= cnt+1
+#		for id1 in idlist:
+#			id2 = id1.replace("(","")
+#			id2 = id2.replace(")","")
+#			id3 = id2.split(", ")
+#			for id4 in id3:
+#				id5 = id4.split("=")
+#				if id5[0]=="tag_id":tagid_A.append(id5[1])
+#				elif id5[0]=="type":type_A.append(id5[1])
+#				elif id5[0]=="antenna": antenna_A.append(id5[1])
+#				elif id5[0]=="repeat": repeat_A.append(id5[1])
+#				cnt= cnt+1
 
-		tagid_B = list()
-		type_B = list()
-		antenna_B = list()
-		repeat_B = list()
+#		tagid_B = list()
+#		type_B = list()
+#		antenna_B = list()
+#		repeat_B = list()
 
-		for loc1 in loclist:
-			loc2 = loc1.replace("(","")
-			loc2 = loc2.replace(")","")
-			loc3 = loc2.split(", ")
-			for loc4 in loc3 :
-				loc5 = loc4.split("=")
-				if loc5[0]=="tag_id": tagid_B.append(loc5[1])
-				elif loc5[0]=="type": type_B.append(loc5[1])
-				elif loc5[0]=="antenna": antenna_B.append(loc5[1])
-				elif loc5[0]=="repeat": repeat_B.append(loc5[1])
-				cnt= cnt+1
+#		for loc1 in loclist:
+#			loc2 = loc1.replace("(","")
+#			loc2 = loc2.replace(")","")
+#			loc3 = loc2.split(", ")
+#			for loc4 in loc3 :
+#				loc5 = loc4.split("=")
+#				if loc5[0]=="tag_id": tagid_B.append(loc5[1])
+#				elif loc5[0]=="type": type_B.append(loc5[1])
+#				elif loc5[0]=="antenna": antenna_B.append(loc5[1])
+#				elif loc5[0]=="repeat": repeat_B.append(loc5[1])
+#				cnt= cnt+1
 
-		repeat_AA = list()
+#		repeat_AA = list()
 
-		for rep_A in repeat_A:
-			repeat_AA.append(int(rep_A))
+#		for rep_A in repeat_A:
+#			repeat_AA.append(int(rep_A))
 
-		if max(repeat_AA) in repeat_AA:
-			n = repeat_AA.index(max(repeat_AA))
+#		if max(repeat_AA) in repeat_AA:
+#			n = repeat_AA.index(max(repeat_AA))
 
-		tagsplt = tagid_A[n].split("AAAA")
-		realtag = int(tagsplt[1][0:4])
+#		tagsplt = tagid_A[n].split("AAAA")
+#		realtag = int(tagsplt[1][0:4])
 
-		soc.close()
+#		soc.close()
 
-#		realtag = 68
+		realtag = 67
 
 # Query database #
 		if realtag != 'None':
-			conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-			cur = conn.cursor()
-			cur.execute("SELECT * FROM `paper_rolldetails` WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s LIMIT 1", realtag)
-			query1 = cur.fetchone()
-			paper_roll_id = query1[0]
-			paper_code = query1[1]
-			initial_weight = query1[4]
-			size = query1[7]
-			uom = query1[8]
+			query1 = PaperRoll.objects.filter(id=realtag).values_list('paper_code', 'width', 'wunit', 'initial_weight', 'lane', 'position')[0]
 
-			cur.execute("SELECT * FROM `paper_movement` WHERE `paper_movement`.`roll_id` = %s ORDER BY `paper_movement`.`created_on` DESC", realtag)
-			query2 = cur.fetchall()
+			paper_code = query1[0]
+			size = query1[1]
+			uom = query1[2]
+			initial_weight = query1[3]
+			lane = query1[4]
+			position = query1[5]
 
-			if len(query2)>0:
-				actual_wt = query2[0][3]
+			query2 = PaperHistory.objects.filter(roll_id=realtag).order_by('-timestamp').values_list('last_wt')
+			exists = PaperHistory.objects.filter(roll_id=realtag).exists()
+
+			int_weight = int(weight)
+
+			if exists == True:
+				actual_wt = query2[0]
 			else:
 				actual_wt = initial_weight
 
@@ -182,11 +174,8 @@ def scale(request):
 				used_weight = actual_wt - weight
 
 # Update temp_weight to database #
-				cur.execute("UPDATE `likitomi_v8`.`paper_rolldetails` SET `temp_weight` = %s WHERE `paper_rolldetails`.`paper_roll_detail_id` = %s", (weight, realtag))
-				conn.commit()
-
-				cur.close()
-				conn.close()
+				p = PaperRoll(id=realtag, paper_code=paper_code, width=size, wunit=uom, initial_weight=initial_weight, temp_weight=int_weight, lane=lane, position=position)
+				p.save()
 			else:
 				used_weight = ""
 
--- mysite/weight/stock_views.py
+++ mysite/weight/stock_views.py
@@ -1,53 +1,24 @@
 # Create your views here.
-from django.template.loader import get_template
 from django.shortcuts import render_to_response
-from django.template import Template, Context
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.db import connection, transaction
-
-import datetime
-import serial
-import MySQLdb
-import Image
-import socket
-import StringIO
-import cStringIO
-import random
 
 def stock(request):
 	try:
-#		conn = MySQLdb.Connect(host="localhost", user="root", passwd="", db="likitomi_v8")
-#		cur = conn.cursor()
-
-#		cur.execute("SELECT DISTINCT `paper_code` FROM `paper_rolldetails`")
-#		query = cur.fetchall()
-#		qlen = len(query)
-#		qstr = str(query)[3:][:-4]
-#		qsplt = qstr.split("',), ('")
-#		pcodelist = list()
-#		for pcode in qsplt:
-#			pcodelist.append(pcode)
-
-#		cur.close()
-#		conn.close()
-
 		if 'pcode' in request.GET and request.GET['pcode']:
 			pcode = request.GET['pcode']
 		else:
 			pcode = ""
-#			return HttpResponseRedirect('/stock/')
 
 		if 'width' in request.GET and request.GET['width']:
 			width = request.GET['width']
 		else:
 			width = ""
-#			return HttpResponseRedirect('/stock/')
 
 		if 'loss' in request.GET and request.GET['loss']:
 			loss = request.GET['loss']
 		else:
 			loss = ""
-#			return HttpResponseRedirect('/stock/')
 
 		cursor = connection.cursor()
 		cursor.execute("""
