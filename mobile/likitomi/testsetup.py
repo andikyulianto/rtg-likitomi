c37aa3465a91781d1cba9a4a194a36daf7db28f9
chanaphan.prasomwong <chanaphan.prasomwong@73fb50ce-e502-64b5-abbe-fc10fc176274>
2010-12-06 14:36:13 +0000

    Add static folder

---
+++ mobile/likitomi/testsetup.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+class ViewTest(TestCase):
+    def setUp(self):
+        app()
---
+++ mobile/likitomi/tryout.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+#from django.template import Template, Context
+
+def index(request):
+    print 'abc'
+    
