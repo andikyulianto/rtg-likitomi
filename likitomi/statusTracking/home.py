a99e1954aceed052d7d0941a1aff711aed76cc5b
chanaphan.prasomwong <chanaphan.prasomwong@73fb50ce-e502-64b5-abbe-fc10fc176274>
2011-04-03 04:14:36 +0000

    update not process and missing

--- likitomi/statusTracking/home.py
+++ likitomi/statusTracking/home.py
@@ -58,10 +58,10 @@ def showPC(eID,title):
 	#temp_contents = extra[0].days_left
 	#item_plan_cr = FakeStatusTracking.objects.filter(plan_cr_start__year= today.year, plan_cr_start__month=today.month, plan_cr_start__day=today.day).values_list("plan_cr_start","plan_cr_end","product_id","actual_cr_start","actual_cr_end").order_by('plan_cr_start')
 	#item_plan_cr = FakeStatusTracking.objects.filter(plan_cr_start__year=
-	item_plan_cv = FakeStatusTracking.objects.filter(plan_cv_start__year=today.year, plan_cv_start__month=today.month, plan_cv_start__day=today.day).values_list("plan_cv_start", "plan_cv_end", "product_id", "actual_cv_start", "actual_cv_end", "cv_machine","process1","plan_due").order_by('plan_cv_start')
-	item_plan_pt = FakeStatusTracking.objects.filter(plan_pt_start__year=today.year, plan_pt_start__month=today.month, plan_pt_start__day=today.day).values_list("plan_pt_start", "plan_pt_end", "product_id", "actual_pt_start", "actual_pt_end","process2","plan_due").order_by('plan_pt_start')
+	item_plan_cv = FakeStatusTracking.objects.filter(plan_cv_start__year=today.year, plan_cv_start__month=today.month, plan_cv_start__day=today.day).values_list("plan_cv_start", "plan_cv_end", "product_id", "actual_cv_start", "actual_cv_end", "cv_machine","process1","plan_due","plan_amount","actual_amount_cv").order_by('plan_cv_start')
+	item_plan_pt = FakeStatusTracking.objects.filter(plan_pt_start__year=today.year, plan_pt_start__month=today.month, plan_pt_start__day=today.day).values_list("plan_pt_start", "plan_pt_end", "product_id", "actual_pt_start", "actual_pt_end","process2","plan_due","plan_amount","actual_amount_cv").order_by('plan_pt_start')
 	#bug here ordering (also in utility line67)
-	item_plan_wh = FakeStatusTracking.objects.filter(plan_wh_start__year=today.year, plan_wh_start__month=today.month, plan_wh_start__day=today.day).values_list("plan_wh_start", "product_id","actual_wh_start","process1","process2","process3","plan_due").order_by('plan_wh_start')
+	item_plan_wh = FakeStatusTracking.objects.filter(plan_wh_start__year=today.year, plan_wh_start__month=today.month, plan_wh_start__day=today.day).values_list("plan_wh_start", "product_id","actual_wh_start","process1","process2","process3","plan_due","plan_amount","actual_amount_wh").order_by('plan_wh_start')
 	
 	items_plan_cr = list(item_plan_cr)
 	items_plan_cv = list(item_plan_cv)
@@ -113,7 +113,108 @@ def showPC(eID,title):
 	endList = startList+getPCItemNum()
 	items_plan_wh=items_plan_wh[startList:endList]
 	#temp_contents = currentProcess("2CL")
+
 	return render_to_response('PC/view.html', locals())
+
+
+def section(request):
+	eID = request.GET['eID']
+	today = todayDate()
+	temp_contents = ''
+
+	title = str(today)
+	is_enable_table = True
+	is_enable_desktop = True
+
+	employee = Employee.objects.get(eid=eID)
+	page =  employee.task
+	#section_title = employee.lastname
+	section_title = "Homepage for " + employee.task + " Login as " + employee.firstname + " " + employee.lastname
+	if(page == "PC"):
+		return showPC(eID,section_title)
+	if(page == "GM"):
+		return showGM(eID,section_title)
+	if(page == "CR"):
+		return workCR(eID,section_title)
+	if(page == "CV"):
+		return workCV(eID,section_title)
+	if(page == "PT"):
+		return workPT(eID,section_title)
+	if(page == "WH"):
+		return workWH(eID,section_title)
+	
+	return render_to_response('home.html', locals())
+
+def PCrefresher(request):
+	today = todayDate()
+	page = "PC"
+	is_enable_leftbutton = True
+	is_enable_rightbutton = True
+	
+
+	#create items for PC
+	#extra = db_type(FakeStatusTracking.objects.all())
+	item_plan_cr = FakeStatusTracking.objects.filter(plan_cr_start__year= today.year, plan_cr_start__month=today.month, plan_cr_start__day=today.day).values_list("plan_cr_start","plan_cr_end","product_id","actual_cr_start","actual_cr_end","days_left","plan_amount","actual_amount_cr").order_by('plan_cr_start')
+	#temp_contents = extra[0].days_left
+	#item_plan_cr = FakeStatusTracking.objects.filter(plan_cr_start__year= today.year, plan_cr_start__month=today.month, plan_cr_start__day=today.day).values_list("plan_cr_start","plan_cr_end","product_id","actual_cr_start","actual_cr_end").order_by('plan_cr_start')
+	#item_plan_cr = FakeStatusTracking.objects.filter(plan_cr_start__year=
+	item_plan_cv = FakeStatusTracking.objects.filter(plan_cv_start__year=today.year, plan_cv_start__month=today.month, plan_cv_start__day=today.day).values_list("plan_cv_start", "plan_cv_end", "product_id", "actual_cv_start", "actual_cv_end", "cv_machine","process1","plan_due","plan_amount","actual_amount_cv").order_by('plan_cv_start')
+	item_plan_pt = FakeStatusTracking.objects.filter(plan_pt_start__year=today.year, plan_pt_start__month=today.month, plan_pt_start__day=today.day).values_list("plan_pt_start", "plan_pt_end", "product_id", "actual_pt_start", "actual_pt_end","process2","plan_due","plan_amount","actual_amount_cv").order_by('plan_pt_start')
+	#bug here ordering (also in utility line67)
+	item_plan_wh = FakeStatusTracking.objects.filter(plan_wh_start__year=today.year, plan_wh_start__month=today.month, plan_wh_start__day=today.day).values_list("plan_wh_start", "product_id","actual_wh_start","process1","process2","process3","plan_due","plan_amount","actual_amount_wh").order_by('plan_wh_start')
+	
+	items_plan_cr = list(item_plan_cr)
+	items_plan_cv = list(item_plan_cv)
+	items_plan_pt = list(item_plan_pt)
+	items_plan_wh = list (item_plan_wh)
+	
+	cr = currentTimeProcess("CR")
+	cv = currentTimeProcess("CV")
+	cvThreeCS = currentTimeProcess("3CS")
+	cvThreeCL = currentTimeProcess("3CL")
+	cvTwoCL = currentTimeProcess("2CL")
+	cvThreeCW = currentTimeProcess("3CW")
+	cvTwoCS = currentTimeProcess("2CS")
+	pt = currentTimeProcess("PT")
+	wh = currentTimeProcess("WH")
+	currentTimeProcess("WH") 
+	
+	#prepare list for CR
+	size = len(items_plan_cr)
+	if(currentProcess("CR")!='idle'):
+		pos = positionOfCurrentProcess("CR",currentProcess("CR")[0][0:8])
+	else :
+		pos = size
+	#temp_contents = currentProcess("CV")
+	startList = returnStartingPoint(pos,size)
+	endList = startList+getPCItemNum()
+	items_plan_cr=items_plan_cr[startList:endList]
+	
+	#prepare list for CV
+	size = len(items_plan_cv)
+	pos = positionOfCurrentProcess("CV",currentProcess("CV")[0][0:8])
+	startList = returnStartingPoint(pos,size)
+	endList = startList+getPCItemNum()
+	items_plan_cv=items_plan_cv[startList:endList]
+	
+	#prepare list for PT
+	size = len(items_plan_pt)
+	pos = positionOfCurrentProcess("PT",currentProcess("PT")[0][0:8])
+	startList = returnStartingPoint(pos,size)
+	endList = startList+getPCItemNum()
+	items_plan_pt=items_plan_pt[startList:endList]
+	
+	#prepare list for WH
+	size = len(items_plan_wh)
+	#pos =currentProcess("WH")[0][0]
+	#temp_contents = currentProcess("WH")[0][0]
+	pos = positionOfCurrentProcess("WH",currentProcess("WH")[0][0])
+	startList = returnStartingPoint(pos,size)
+	endList = startList+getPCItemNum()
+	items_plan_wh=items_plan_wh[startList:endList]
+	#temp_contents = currentProcess("2CL")
+
+	return render_to_response('PC/PC.html', locals())
 	
 ###################################################
 ##                 for manager                   ##
