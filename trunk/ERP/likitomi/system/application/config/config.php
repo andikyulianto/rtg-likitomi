5b73017eedf67b76c27c331e61a8359eed1d17f9
chanaphan.prasomwong <chanaphan.prasomwong@73fb50ce-e502-64b5-abbe-fc10fc176274>
2011-05-06 18:39:27 +0000

    update charset

--- trunk/ERP/likitomi/system/application/config/config.php
+++ trunk/ERP/likitomi/system/application/config/config.php
@@ -77,7 +77,7 @@ $config['language']	= "english";
 | that require a character set to be provided.
 |
 */
-$config['charset'] = "UTF-8";
+$config['charset'] = "TIS-620";
 
 /*
 |--------------------------------------------------------------------------
