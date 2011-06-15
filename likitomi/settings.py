9a69f2becd6cf41c4d22a039915e85c2bf3e86a0
pati.pik <pati.pik@73fb50ce-e502-64b5-abbe-fc10fc176274>
2011-06-15 17:09:57 +0000

    cleanup

--- likitomi/settings.py
+++ likitomi/settings.py
@@ -96,16 +96,16 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-#    'django.contrib.sessions',
-#    'django.contrib.sites',
-#    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
     'likitomi.statusTracking',
     'likitomi.weight',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'test_utils',
+#    'test_utils',
 )
 
 FIXTURE_DIRS = (
