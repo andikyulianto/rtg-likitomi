49b87e54c09d37d3ef9a16cffcee7d03a9dd0461
pati.pik <pati.pik@73fb50ce-e502-64b5-abbe-fc10fc176274>
2011-06-22 08:51:07 +0000

    pyc

--- trunk/likitomi/settings.py
+++ trunk/likitomi/settings.py
@@ -112,4 +112,71 @@ FIXTURE_DIRS = (
 #   '/home/fon/Django-123/projects/rtg-likitomi/likitomi/statusTracking/fixtures/',
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+#    'filters': {
+#        'special': {
+#            '()': 'project.logging.SpecialFilter',
+#            'foo': 'bar',
+#        }
+#    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+#            'filters': ['special']
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'myproject.custom': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+#            'filters': ['special']
+        }
+    }
+}
 
+import logging
+
+if DEBUG:
+    # will output to your console
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = '%(asctime)s %(levelname)s %(message)s',
+    )
+else:
+    # will output to logging file
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = '%(asctime)s %(levelname)s %(message)s',
+        filename = '/my_log_file.log',
+        filemode = 'a'
+    )
--- trunk/likitomi/weight/longtry.py
+++ trunk/likitomi/weight/longtry.py
@@ -9,10 +9,17 @@ import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+from django.conf import settings
+
 def orient(request):
 	return render_to_response('orient.html', locals())
 
 def longtry(request):
-	longtry = 'longtry'
+	logging.basicConfig(filename='exceptions.log', level=logging.DEBUG)
+
+	try:
+		1/0
+	except ZeroDivisionError as e:
+		logging.debug(e)
 
 	return render_to_response('longtry.html', locals())
