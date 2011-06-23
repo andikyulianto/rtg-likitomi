af3c7fccff34f5deaba1fc9fca3ed1ff67abdfb1
pati.pik <pati.pik@73fb50ce-e502-64b5-abbe-fc10fc176274>
2011-06-23 05:29:20 +0000

    logging to file

--- likitomi/weight/longtry.py
+++ likitomi/weight/longtry.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 # import the logging library
 import logging
+import traceback
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -15,11 +16,12 @@ def orient(request):
 	return render_to_response('orient.html', locals())
 
 def longtry(request):
-	logging.basicConfig(filename='exceptions.log', level=logging.DEBUG)
+	logging.basicConfig(filename='/home/patipol/rtg-likitomi/likitomi/static/logfile.log', level=logging.DEBUG)
 
 	try:
 		1/0
 	except ZeroDivisionError as e:
-		logging.debug(e)
+		logging.error(str(e) +" by requesting \"./longtry/\" at "+ datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		logging.error(traceback.print_exc())
 
 	return render_to_response('longtry.html', locals())
